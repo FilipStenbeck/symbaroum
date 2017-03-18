7ef2b2446fd8c2dceb6f1cee92880340f25a94c0
Filip Stenbeck <filip.stenbeck@svt.se>
2017-03-18 10:25:44 +0100

    added logo

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -11,29 +11,47 @@ import {
 const Header = ({choosen}) => {
     if (choosen === 'chronicle') {
         return (
+            <div>
+
+            <div className="logo-container">
+                <img className="logo" src="http://www.prometheuscrowdfunding.de/files/2016/06/symbaroum_logo_600px.png"/>
+            </div>
             <nav className={header}>
                 <span><Link className={selected} to='/'>Krönika</Link></span>
                 <span><Link className={link} to='/npc'>Personer</Link></span>
 
             </nav>
+        </div>
     	);
     }
     if (choosen === 'npc') {
         return (
+            <div>
+
+            <div className="logo-container">
+                <img className="logo" src="http://www.prometheuscrowdfunding.de/files/2016/06/symbaroum_logo_600px.png"/>
+            </div>
             <nav className={header}>
                 <span><Link className={link} to='/'>Krönika</Link></span>
                 <span><Link className={selected} to='/npc'>Personer</Link></span>
 
             </nav>
+        </div>
     	);
     }
 
     return (
+        <div>
+
+        <div className="logo-container">
+            <img className="logo" src="http://www.prometheuscrowdfunding.de/files/2016/06/symbaroum_logo_600px.png"/>
+        </div>
         <nav className={header}>
             <span><Link className={link} to='/'>Krönika</Link></span>
             <span><Link className={link + ' ' + selected} to='/npc'>Personer</Link></span>
 
         </nav>
+    </div>
     );
 };
 
