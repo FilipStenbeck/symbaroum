0fc7d89967f58a8d644e357a92394e251fd234f4
Filip Stenbeck <filip.stenbeck@nordnet.se>
2018-01-30 08:45:22 +0100

    naming the image at runtime

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -p 9000:9000 --label traefik.docker.network=traefik --name=symbaroum -d filipstenbeck/symbaroum
+echo "Enter name:"
+read name
+docker run -p 9000:9000 --label traefik.docker.network=traefik --name=$name -d filipstenbeck/symbaroum
